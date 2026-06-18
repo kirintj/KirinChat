29e401959534de59031fb1c00ff1a0b71f6a59db
kirin <2896651097@qq.com>
2026-06-18 12:10:02 +0800

    feat(migrate): remove ElScrollbar from utils

--- src/frontend/src/utils/function.ts
+++ src/frontend/src/utils/function.ts
@@ -1,41 +1,33 @@
-import { nextTick, ref } from "vue"
-import { ElScrollbar } from "element-plus"
+import { nextTick } from "vue"
 import { ChatMessage } from "../type"
-
-const scrollbar = ref<InstanceType<typeof ElScrollbar>>()
 
 export function getHistoryChat(list: any, chatArr: ChatMessage[]) {
   for (let i = 0; i < list.data.data.length; i += 2) {
-    const chatMsg = ref<ChatMessage>({
+    const chatMsg: ChatMessage = {
       personMessage: { content: '' },
       aiMessage: { content: '' }
-    })
+    }
     if (list.data.data[i].role === 'user') {
-      chatMsg.value.personMessage.content = list.data.data[i].content
+      chatMsg.personMessage.content = list.data.data[i].content
     }
     if (list.data.data[i + 1].role === 'user') {
-      chatMsg.value.personMessage.content = list.data.data[i + 1].content
+      chatMsg.personMessage.content = list.data.data[i + 1].content
     }
     if (list.data.data[i].role === 'assistant') {
-      chatMsg.value.aiMessage.content = list.data.data[i].content
+      chatMsg.aiMessage.content = list.data.data[i].content
     }
     if (list.data.data[i + 1].role === 'assistant') {
-      chatMsg.value.aiMessage.content = list.data.data[i + 1].content
+      chatMsg.aiMessage.content = list.data.data[i + 1].content
     }
-    chatArr.push(chatMsg.value)
+    chatArr.push(chatMsg)
     scrollBottom()
   }
 }
 
-export function scrollBottom() {
-  // 在下次DOM更新循环结束后执行滚动到底部的操作
+export function scrollBottom(container?: HTMLElement | null) {
   nextTick(() => {
-    // 确保scrollbar对象存在
-    if (scrollbar.value) {
-      const wrapEl = scrollbar.value.wrapRef // 获取滚动容器元素
-        ; (wrapEl as HTMLDivElement).scrollTop = (
-          wrapEl as HTMLDivElement
-        ).scrollHeight // 直接设置scrollTop滚动到底部
+    if (container) {
+      container.scrollTop = container.scrollHeight
     }
   })
 }
